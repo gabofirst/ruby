1af5056e0660d447f8a40855f375006f967e0e4d
gabofirst <gabof93@gmail.com>
2017-08-21 16:33:54 -0400

    comiit myapp

---
+++ README.md
@@ -0,0 +1,3 @@
+# Proyecto guía/ejemplo para principiantes con Ruby on Rails
+
+Este proyecto pretende ser una guía para quienes quieran aprender a programar con Ruby on Rails. Ayuda bastante si vienes de otro lenguaje de programación como Java o PHP, también si sabes de HTML, CSS o Javascript, pero no te preocupes si no sabes que son esas cosas, la idea es ir explicando todos esos puntos.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProyectoEjemplo::Application.config.secret_token = 'ed50e71d14dd26d257825f2acfe7d7d599d66143446101406356ba4dbc423796cf89ac4fa98adec5d87375dc98dabb0c332df8c5b9b99511e477a9ef2a38d2b2'
